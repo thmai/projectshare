d5bf3e537453979884ea6ca7db9c78b3a0125d1b
Shubham Goyal <shubham.goyal@live.in>
2016-01-30 15:03:08 +0800

    Store translations map to prevent querying again

--- Translations/translate.py
+++ Translations/translate.py
@@ -16,12 +16,29 @@
 
 import googleapiclient
 from googleapiclient.discovery import build
+import pickle
+import argparse
 
-f = open('input.txt', 'r')
-f_str = f.read()
-f_list = f_str.split('\n')
+MAP_BLAH_TO_ENGLISH = None
 
-def main():
+try:
+	file_pickle = open('map.donottouch', 'rb')
+	MAP_BLAH_TO_ENGLISH = pickle.load(file_pickle)
+except FileNotFoundError as e:
+	MAP_BLAH_TO_ENGLISH = {}
+	pickle.dump(MAP_BLAH_TO_ENGLISH, open("map.donottouch", "wb" ))
+
+def open_file_and_split_into_list(path):
+	f = open(path, 'r')
+	f_str = f.read()
+	f_list = f_str.split('\n')
+	return f_list
+
+def store_in_map_and_print(map, key, value):
+	map[key] = value
+	print(value)
+
+def main(list_strings):
 
   # Build a service object for interacting with the API. Visit
   # the Google APIs Console <http://code.google.com/apis/console>
@@ -30,26 +47,37 @@ def main():
             developerKey='AIzaSyBERnUYy4_-ZTTZkv_9ReT2mGlyHG0-9qY')
 
 # q = f_list
-	for text_original in f_list:
-		response_detection = service.detections().list(q = [text_original]).execute()
-		response_detection_text = response_detection['detections'][0]
-		if len(response_detection_text) == 1:
-			# if not response_detection_text[0]['isReliable']:
-			# 	print('Not Reliable')
-			# else:
-			try:
-				if response_detection_text[0]['language'] == 'en':
-					print(text_original)
-				else:
-					translations = service.translations().list(source=response_detection_text[0]['language'], target='en', q=[text_original]).execute()['translations']
-					if len(translations) > 1:
-						print('More than 1 translation possible')
-					else:
-						print(translations[0]['translatedText'])
-			except googleapiclient.errors.HttpError as e:
-				print(e)
+	for text_original in list_strings:
+		if text_original in MAP_BLAH_TO_ENGLISH:
+			print(MAP_BLAH_TO_ENGLISH[text_original])
+		elif not text_original.strip():
+			store_in_map_and_print(MAP_BLAH_TO_ENGLISH, text_original, text_original)
 		else:
-			print('More than 1 language possible')
+			try:
+				val = int(text_original.strip())
+				store_in_map_and_print(MAP_BLAH_TO_ENGLISH, text_original, text_original)
+			except ValueError:
+				response_detection = service.detections().list(q = [text_original]).execute()
+				response_detection_text = response_detection['detections'][0]
+				if len(response_detection_text) == 1:
+					# if not response_detection_text[0]['isReliable']:
+					# 	print('Not Reliable')
+					# else:
+					try:
+						if response_detection_text[0]['language'] == 'en':
+							print(text_original)
+						else:
+							translations = service.translations().list(source=response_detection_text[0]['language'], target='en', q=[text_original]).execute()['translations']
+							if len(translations) > 1:
+								print('More than 1 translation possible')
+							else:
+								MAP_BLAH_TO_ENGLISH[text_original] = translations[0]['translatedText']
+								print(translations[0]['translatedText'])
+					except googleapiclient.errors.HttpError as e:
+						print(e)
+				else:
+					print('More than 1 language possible')
+	pickle.dump(MAP_BLAH_TO_ENGLISH, open("map.donottouch", "wb" ))
 
 
 
@@ -60,4 +88,8 @@ def main():
 	#     ).execute())
 
 if __name__ == '__main__':
-  main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input", help="input file with pieces of text to be translated in different lines")
+	args = parser.parse_args()
+	f_list = open_file_and_split_into_list(args.input)
+	main(f_list)
